90f6c42a11dbfc7e99b3a3261d07a4420c4549ec
Adam <algolec1990@yahoo.com>
2023-07-01 15:13:20 -0400

    first commit

---
+++ Source/MyProject/MyObject.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyObject.h"
+
+UMyObject::UMyObject() {
+	myFloat = 0.f;
+}
+
+void UMyObject::MyFunction() {
+	UE_LOG(LogTemp, Warning, TEXT("This is our warning text!"));
+}
